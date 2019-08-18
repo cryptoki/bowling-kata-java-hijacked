0a3cafb6445629bf11529a3ef60de62b6012df0e
Steffen Haase <steffen.haase@epost-dev.de>
2019-08-18 13:57:50 +0200

    add test for game from the exercise sheet

--- src/test/java/kata/bowling/BowlingGameTest.java
+++ src/test/java/kata/bowling/BowlingGameTest.java
@@ -50,6 +50,12 @@ public class BowlingGameTest {
         assertEquals(26, bowlingGame.score());
     }
 
+    @Test
+    void canScoreGameInExerciseSheet() {
+        rollWith(1,4,  4,5,  6,4,  5,5,  10,  0,1,  7,3,  6,4,  10,  2,8,6);
+        assertEquals(133, bowlingGame.score());
+    }
+
     private void rollWithTwentyTimes(int pinsDown) {
         for (int i = 0; i < 20; i++) {
             bowlingGame.roll(pinsDown);
