73072710fc4bddd8f64eab873ae1bab3efadee26
Steffen Haase <steffen.haase@epost-dev.de>
2019-08-18 13:17:32 +0200

    refactoring, better way to hold the rolls in an array. it can be max 21 rolls I FORGOT to push the other commits :(

--- src/main/java/kata/bowling/BowlingGame.java
+++ src/main/java/kata/bowling/BowlingGame.java
@@ -1,14 +1,17 @@
 package kata.bowling;
 
+import java.util.stream.IntStream;
+
 public class BowlingGame {
 
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int index = 0;
 
     public void roll(int pinsDown) {
-        score += pinsDown;
+        rolls[index++] = pinsDown;
     }
 
     public int score() {
-        return score;
+        return IntStream.of(rolls).sum();
     }
 }
