f435d5b25f44cb2936a52d2d809f44eeb695dbe8
Steffen Haase <steffen.haase@epost-dev.de>
2019-08-18 13:34:40 +0200

    refactoring for spare method and constant

--- src/main/java/kata/bowling/BowlingGame.java
+++ src/main/java/kata/bowling/BowlingGame.java
@@ -2,6 +2,7 @@ package kata.bowling;
 
 public class BowlingGame {
 
+    private static final int MAX_PINS = 10;
     private int[] rolls = new int[21];
     private int index = 0;
 
@@ -13,7 +14,7 @@ public class BowlingGame {
         int score = 0;
         int cursorInRolls = 0;
         for (int frame = 1; frame <= 10; frame++) {
-            if (rolls[cursorInRolls] + rolls[cursorInRolls + 1] == 10) {
+            if (isSpareFrame(cursorInRolls)) {
                 score += 10 + rolls[cursorInRolls + 2];
             } else {
                 score += rolls[cursorInRolls] + rolls[cursorInRolls + 1];
@@ -21,5 +22,9 @@ public class BowlingGame {
             cursorInRolls += 2;
         }
         return score;
+    }
+
+    private boolean isSpareFrame(int cursorInRolls) {
+        return rolls[cursorInRolls] + rolls[cursorInRolls + 1] == MAX_PINS;
     }
 }
